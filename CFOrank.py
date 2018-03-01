7fa147a7859186655461e1e16953c27e9ea2704b
jfoley12 <jfoley12@LAMU02T9441GTFM.uhc.com>
2018-03-01 10:48:57 -0500

    18.1 done

--- CFOrank.py
+++ CFOrank.py
@@ -6,18 +6,31 @@ jim_foley_url = 'https://games.crossfit.com/competitions/api/v1/competitions/ope
 #Url Request
 r = requests.get(jim_foley_url)
 
+#saving json response a text file to local
 with open('rank.txt', 'w') as fd:
     fd.write(r.text)
 
+#taking all quotes out of text file and saving a second one
+with open('rank.txt', 'r') as fe, open('rank2.txt', 'w') as new:
+    for line in fe:
+        new.write(line.replace('"', '').replace("'", ""))
+
+#parsing txt file to just find my name
 from itertools import islice
 
-with open("rank.txt") as f:
+with open("rank2.txt") as f:
     for line in f:
         if "Jim Foley" in line:
             a=''.join(islice(f,47))
 
+#parsing txt again to just display overall rank
 for item in a.split("\n"):
     if "overallRank" in item:
         rank=item.strip()
 
-print("Jim Foley, you are ranked ", rank[14:20])
+#substring to just include number from line
+b = rank[12:16]
+
+
+print('Jim Foley Overall Rank:', b)
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Crossfit
+Specific to my profile but will open it up to include anyone who wants it.
